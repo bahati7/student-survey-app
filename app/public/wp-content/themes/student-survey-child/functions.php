cdf58596c733dcd37ff03d553aa5d0c7f2424720
Bahati Philemon <bahatiphilmon8@gmail.com>
2025-08-22 11:48:47 +0200

    update functions.php

--- app/public/wp-content/themes/student-survey-child/functions.php
+++ app/public/wp-content/themes/student-survey-child/functions.php
@@ -7,7 +7,7 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-?>
+
 
 
 // Chargement modulaire des fonctionnalités du thème
@@ -15,3 +15,5 @@ require_once get_stylesheet_directory() . '/inc/enqueue.php';
 require_once get_stylesheet_directory() . '/inc/roles.php';
 require_once get_stylesheet_directory() . '/inc/auth-redirect.php';
 require_once get_stylesheet_directory() . '/inc/dynamic-menu.php';
+
+?>
