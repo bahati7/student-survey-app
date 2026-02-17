ad6fad931bbe01d726a5a9564b139a4a52999c35
efatha <efathabyamungu4@gmail.com>
2026-02-17 22:40:45 +0300

    Exported SQL file to resolve 0-byte issue and restore database content #60
    The SQL file previously committed to the repository contained 0-byte content, which caused the database to load without tables or records.
    
    This issue was caused by an incomplete database export process, resulting in an empty SQL file being uploaded.
    
    Solution Implemented:
    - Re-exported the database correctly using phpMyAdmin.
    - Verified that the SQL file now contains a valid schema and data.
    - Confirmed the file size is no longer 0 bytes.
    - Replaced the empty SQL file in the project.
    - Imported and tested the database locally to ensure proper functionality.
    
    Result:
    The database now loads successfully with the full schema and data.
    Application content displays correctly.
    The issue of the empty file has been resolved.

---
+++ wp-content/.data/wp_usermeta.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2026 at 08:18 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_survey_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_usermeta`
+--
+
+CREATE TABLE `wp_usermeta` (
+  `umeta_id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL DEFAULT 0,
+  `meta_key` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
+  `meta_value` longtext COLLATE utf8mb4_unicode_520_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Dumping data for table `wp_usermeta`
+--
+
+INSERT INTO `wp_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) VALUES
+(1, 1, 'nickname', 'admin'),
+(2, 1, 'first_name', ''),
+(3, 1, 'last_name', ''),
+(4, 1, 'description', ''),
+(5, 1, 'rich_editing', 'true'),
+(6, 1, 'syntax_highlighting', 'true'),
+(7, 1, 'comment_shortcuts', 'false'),
+(8, 1, 'admin_color', 'fresh'),
+(9, 1, 'use_ssl', '0'),
+(10, 1, 'show_admin_bar_front', 'true'),
+(11, 1, 'locale', ''),
+(12, 1, 'wp_capabilities', 'a:1:{s:13:\"administrator\";b:1;}'),
+(13, 1, 'wp_user_level', '10'),
+(14, 1, 'dismissed_wp_pointers', ''),
+(15, 1, 'show_welcome_panel', '1'),
+(17, 1, 'wp_dashboard_quick_press_last_post_id', '36'),
+(18, 1, 'wp_persisted_preferences', 'a:3:{s:4:\"core\";a:2:{s:26:\"isComplementaryAreaVisible\";b:0;s:24:\"enableChoosePatternModal\";b:1;}s:14:\"core/edit-post\";a:1:{s:12:\"welcomeGuide\";b:0;}s:9:\"_modified\";s:24:\"2026-02-11T19:15:53.887Z\";}'),
+(19, 1, 'session_tokens', 'a:1:{s:64:\"12d2a9fe27d4ed742f3db79306f48af976fc6746b583f1aa195ec1bc439f6b73\";a:4:{s:10:\"expiration\";i:1771521852;s:2:\"ip\";s:3:\"::1\";s:2:\"ua\";s:111:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36\";s:5:\"login\";i:1771349052;}}'),
+(20, 1, 'closedpostboxes_question', 'a:0:{}'),
+(21, 1, 'metaboxhidden_question', 'a:1:{i:0;s:7:\"slugdiv\";}'),
+(22, 1, 'meta-box-order_question', 'a:3:{s:4:\"side\";s:9:\"submitdiv\";s:6:\"normal\";s:24:\"question_details,slugdiv\";s:8:\"advanced\";s:0:\"\";}'),
+(23, 1, 'screen_layout_question', '2'),
+(24, 2, 'nickname', 'Instructor'),
+(25, 2, 'first_name', 'Efatha'),
+(26, 2, 'last_name', 'Rutakaza'),
+(27, 2, 'description', ''),
+(28, 2, 'rich_editing', 'true'),
+(29, 2, 'syntax_highlighting', 'true'),
+(30, 2, 'comment_shortcuts', 'false'),
+(31, 2, 'admin_color', 'fresh'),
+(32, 2, 'use_ssl', '0'),
+(33, 2, 'show_admin_bar_front', 'true'),
+(34, 2, 'locale', ''),
+(35, 2, 'wp_capabilities', 'a:1:{s:7:\"student\";b:1;}'),
+(36, 2, 'wp_user_level', '0'),
+(37, 2, 'dismissed_wp_pointers', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `wp_usermeta`
+--
+ALTER TABLE `wp_usermeta`
+  ADD PRIMARY KEY (`umeta_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `meta_key` (`meta_key`(191));
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `wp_usermeta`
+--
+ALTER TABLE `wp_usermeta`
+  MODIFY `umeta_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ wp-content/.data/wp_users.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2026 at 08:19 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_survey_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_users`
+--
+
+CREATE TABLE `wp_users` (
+  `ID` bigint(20) UNSIGNED NOT NULL,
+  `user_login` varchar(60) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `user_pass` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `user_nicename` varchar(50) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `user_email` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `user_url` varchar(100) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `user_registered` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `user_activation_key` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
+  `user_status` int(11) NOT NULL DEFAULT 0,
+  `display_name` varchar(250) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+--
+-- Dumping data for table `wp_users`
+--
+
+INSERT INTO `wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`) VALUES
+(1, 'admin', '$wp$2y$10$IfHyiFAq0q6.VpYEK5SIuukdrQklAlzx/3VSzYLApCIWKrXJV3IDC', 'admin', 'efathabyamungu4@gmail.com', 'http://localhost/student-survey-app', '2026-02-02 19:22:15', '', 0, 'admin'),
+(2, 'Instructor', '$wp$2y$10$fFYs.B1.rSjyzP2Ni98dmu3/gSeqZhfeLz9b0UYmjzx1Jz06KYcGm', 'instructor', 'efatharutakaza@gmail.com', 'https://efatha.github.io/my-portofolio/', '2026-02-17 17:39:17', '1771349957:$generic$cBZrR4eN2t-jcriNLMRLJ7y61PuA1cnMDhvR5oLI', 0, 'Efatha Rutakaza');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `wp_users`
+--
+ALTER TABLE `wp_users`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `user_login_key` (`user_login`),
+  ADD KEY `user_nicename` (`user_nicename`),
+  ADD KEY `user_email` (`user_email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `wp_users`
+--
+ALTER TABLE `wp_users`
+  MODIFY `ID` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
