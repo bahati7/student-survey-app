95279ee2c5c9d339b17abda4693e65c32a2b15fa
Samuel BITINGINGWA <98274760+Samuel11s@users.noreply.github.com>
2026-03-01 22:07:46 +0300

    chore(dashboard-page): adding the header and footer and getting the current user ID and role

--- wp-content/themes/student-survey-child/page-dashboard.php
+++ wp-content/themes/student-survey-child/page-dashboard.php
@@ -3,3 +3,24 @@
     Template for Dashboard page
     Template Name: Dashboard
 */
+    if (!is_user_logged_in()) {
+        wp_redirect(wp_login_url());
+        exit;
+    }
+
+    get_header();
+    
+    $current_user = wp_get_current_user();
+    $user_id = $current_user->ID;
+    $user_roles = $current_user->roles;
+
+    $is_student = in_array('student', $user_roles);
+    $is_instructor = in_array('instructor', $user_roles);
+
+?>
+
+
+
+<?php 
+    get_footer(); 
+?>
